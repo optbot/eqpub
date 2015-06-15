959ff7d8525bf62c3dd877b7c876ced6d52d7bc1
Marshall Farrier <info@marshallfarrier.com>
2015-06-14 23:08:14 -0700

    Logging.

--- src/eqpub.py
+++ src/eqpub.py
@@ -17,23 +17,23 @@ import constants
 class EqPub(object):
     def __init__(self):
         _config = ConfigParser.SafeConfigParser()
-        _section = constants.CFGSECTION_MAIN
         _config.read(constants.CONFIGFILE)
-        #self._setup_logging(_config)
+        self._setup_logging(_config)
         signal.signal(signal.SIGTERM, self._stop_handler)
 
     def start(self):
-        #self.logger.info('starting')
+        self.logger.info('starting')
         signal.pause()
 
     def _stop_handler(self):
-        #self.logger.info('stopping')
+        self.logger.info('stopping')
         sys.exit(0)
 
     def _setup_logging(self, config):
         self.logger = logging.getLogger(constants.USER)
-        _logpath = _config.get(_section, 'logpath', 1) 
-        _logfmt = _config.get(_section, 'logfmt', 1)
+        _section = constants.CFGSECTION_MAIN
+        _logpath = config.get(_section, 'logpath', 1) 
+        _logfmt = config.get(_section, 'logfmt', 1)
         _handler = logging.FileHandler(os.path.join(_logpath, 'service.log'))
         _formatter = logging.Formatter(_logfmt)
         _handler.setFormatter(_formatter)
